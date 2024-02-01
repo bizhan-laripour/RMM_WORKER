9f8badb2250a6ffd83e2cfee8a14540102575b12
Bizhan <bizhan.laripour.herat@gmail.com>
2024-02-01 02:40:40 -0500

    docker-compose.yml for worker

--- src/main/java/com/rmm/worker/conf/ImperativeElasticsearchConfig.java
+++ src/main/java/com/rmm/worker/conf/ImperativeElasticsearchConfig.java
@@ -1,17 +1,24 @@
 package com.rmm.worker.conf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 
 @Configuration
 public class ImperativeElasticsearchConfig extends ElasticsearchConfiguration {
 
+    private final Environment environment;
+
+    public ImperativeElasticsearchConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     public ClientConfiguration clientConfiguration() {
 
         return ClientConfiguration.builder()
-                .connectedTo("127.0.0.1:9200")
+                .connectedTo(environment.getProperty("elastic.server.port"))
                 .build();
     }
 }
